af2f1bafb6d0126dfca9846ba1e4d2a4b272df6d
tabureanu3 <tabureanulavinia188@gmail.com>
2025-09-12 11:12:25 +0300

    1

---
+++ apple.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "Point.hpp"
+
+class Apple {
+public:
+    Point pos;
+    Apple();
+    Apple(int x, int y);
+};
---
+++ board.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class Board {
+   int _width;
+   int _height;
+public:
+   Board(int width = 35, int height = 35);
+   int GetWidth() const;
+   int GetHeight() const;
+};
