ee17d5d62e709ba14ba86a8b89743492da8678ee
tabureanu3 <tabureanulavinia188@gmail.com>
2025-09-19 08:19:28 +0300

    Update snake.hpp

--- snake.hpp
+++ snake.hpp
@@ -3,6 +3,6 @@
 #include "Point.hpp"
 
 struct Snake {
-    Point segments[50]; // șarpele are max 50 de segmente
-    int length;         // câte segmente are acum
+    Point segments[50];
+    int length;         
 };
