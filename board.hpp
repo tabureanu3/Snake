867c0649d1a9df522936989aeaa49707c84dbc12
tabureanu3 <tabureanulavinia188@gmail.com>
2025-10-15 19:03:48 +0300

    Revert "lab03 Constructori si Operatori"

--- board.hpp
+++ board.hpp
@@ -1,20 +1,7 @@
 #pragma once
-#include "Renderable.hpp"
-#include <iostream>
 
-struct Board : public Renderable {
+struct Board {
     int width;
     int height;
-
     Board(int w = 20, int h = 20);
-    Board(const Board& b);
-    Board& operator=(const Board& b);
-
-    bool operator==(const Board& b) const;
-    bool operator!=(const Board& b) const;
-
-    void render() const override;
 };
-
-std::istream& operator>>(std::istream& in, Board& b);
-std::ostream& operator<<(std::ostream& out, const Board& b);
