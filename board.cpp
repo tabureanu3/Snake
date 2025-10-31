9c76b94fb23532c89d9b98d0c69ea283e158f508
tabureanu3 <tabureanulavinia188@gmail.com>
2025-10-31 08:05:22 +0200

    lab04

--- board.cpp
+++ board.cpp
@@ -1,16 +1,26 @@
 #include "Board.hpp"
-#include <algorithm>
+#include <algorithm>  
 
 Board::Board(int w, int h) : width(w), height(h) {}
 
 void Board::AddApple(const Point& p) {
     apples.push_back(p);
+
+    std::sort(apples.begin(), apples.end(), [](const Point& a, const Point& b) {
+        if (a.x == b.x)
+            return a.y < b.y;
+        return a.x < b.x;
+    });
 }
 
 bool Board::HasApple(const Point& p) const {
-    return std::find(apples.begin(), apples.end(), p) != apples.end();
+    return std::any_of(apples.begin(), apples.end(),
+                       [&](const Point& a) { return a == p; });
 }
 
 void Board::RemoveApple(const Point& p) {
-    apples.erase(std::remove(apples.begin(), apples.end(), p), apples.end());
+
+    apples.erase(std::remove_if(apples.begin(), apples.end(),
+                                [&](const Point& a) { return a == p; }),
+                 apples.end());
 }
